7ef2340555308602bcd6caccfa0e7812191f4c5c
irwinarruda <arruda.irwin@gmail.com>
2025-06-30 22:27:08 -0300

    feat: add first migration CreateUsers

---
+++ Infra/Migrations/20250701010208_CreateUsers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheChatbot.Infra.Migrations {
+  /// <inheritdoc />
+  public partial class CreateUsers : Migration {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) {
+      migrationBuilder.CreateTable(
+        name: "users",
+        columns: (table) => new {
+          Id = table.Column<Guid>(type: "uuid", nullable: false),
+          Name = table.Column<string>(type: "varchar(30)", nullable: false),
+        },
+        constraints: (table) => {
+          table.PrimaryKey("PK_users", x => x.Id);
+        }
+      );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) {
+      migrationBuilder.DropTable(name: "users");
+    }
+  }
+}
