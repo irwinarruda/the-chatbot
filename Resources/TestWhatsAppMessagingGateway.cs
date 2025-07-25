15e65f268b4c3b0f215c5050c6361ba780db50f6
irwinarruda <arruda.irwin@gmail.com>
2025-07-24 23:36:28 -0300

    test: add first test for the SendMessage service

--- Resources/TestWhatsAppMessagingGateway.cs
+++ Resources/TestWhatsAppMessagingGateway.cs
@@ -9,7 +9,7 @@ public class TestWhatsAppMessagingGateway : IWhatsAppMessagingGateway {
 
   public Task SendTextMessage(SendTextMessageDTO textMessage) {
     var phoneNumber = "+5511984444444";
-    SendToSubscribers(new ReceiveTextMessageDTO {
+    NotifySubscribers(new ReceiveTextMessageDTO {
       From = phoneNumber,
       Text = "The message sent was responded"
     });
@@ -17,24 +17,24 @@ public class TestWhatsAppMessagingGateway : IWhatsAppMessagingGateway {
   }
 
   public async Task StartReceiveMessage() {
-    var phoneNumber = "+5511984444444";
-    var mockMessages = new[] {
-      "First message",
-      "Second message",
-      "Third message",
-      "Another one",
-    };
-    foreach (var message in mockMessages) {
-      await Task.Delay(50);
-      SendToSubscribers(new ReceiveTextMessageDTO {
-        From = phoneNumber,
-        Text = message
-      });
-    }
-    SendToSubscribers(new ReceiveTextMessageDTO {
-      From = "+556399999999",
-      Text = "Phone number that does not exist"
-    });
+    // var phoneNumber = "+5511984444444";
+    // var mockMessages = new[] {
+    //   "First message",
+    //   "Second message",
+    //   "Third message",
+    //   "Another one",
+    // };
+    // foreach (var message in mockMessages) {
+    //   await Task.Delay(50);
+    //   NotifySubscribers(new ReceiveTextMessageDTO {
+    //     From = phoneNumber,
+    //     Text = message
+    //   });
+    // }
+    // NotifySubscribers(new ReceiveTextMessageDTO {
+    //   From = "+556399999999",
+    //   Text = "Phone number that does not exist"
+    // });
   }
 
   public Action SubscribeToReceiveMessage(Func<ReceiveTextMessageDTO, Task> onMessageReceived) {
@@ -42,7 +42,7 @@ public class TestWhatsAppMessagingGateway : IWhatsAppMessagingGateway {
     return () => subscribers.Remove(onMessageReceived);
   }
 
-  private void SendToSubscribers(ReceiveTextMessageDTO receiveMessage) {
+  private void NotifySubscribers(ReceiveTextMessageDTO receiveMessage) {
     foreach (var subscriber in subscribers) {
       subscriber.Invoke(receiveMessage);
     }
--- Services/MessagingService.cs
+++ Services/MessagingService.cs
@@ -1,21 +1,33 @@
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+using TheChatbot.Entities;
+using TheChatbot.Infra;
 using TheChatbot.Resources;
 
 namespace TheChatbot.Services;
 
 public class MessagingService {
+  private readonly AppDbContext database;
   private readonly IWhatsAppMessagingGateway whatsAppMessagingGateway;
-  public MessagingService(IWhatsAppMessagingGateway _whatsAppMessagingGateway) {
-    whatsAppMessagingGateway = _whatsAppMessagingGateway;
+  private readonly AuthService authService;
+  public MessagingService(AppDbContext _database, IWhatsAppMessagingGateway _whatsAppMessagingGateway, AuthService _authService) {
     _whatsAppMessagingGateway.SubscribeToReceiveMessage(ListenToReceiveMessage);
+    whatsAppMessagingGateway = _whatsAppMessagingGateway;
+    authService = _authService;
+    database = _database;
   }
-  async Task SendMessage(string phoneNumber, string text) {
+  public async Task SendMessage(string phoneNumber, string text) {
     await whatsAppMessagingGateway.SendTextMessage(new SendTextMessageDTO {
       To = phoneNumber,
       Text = text
     });
   }
 
-  async Task ListenToReceiveMessage(ReceiveTextMessageDTO receiveTextMessage) {
-    throw new NotImplementedException();
+  public async Task ListenToReceiveMessage(ReceiveTextMessageDTO receiveTextMessage) {
+    await authService.GetUserByPhoneNumber(receiveTextMessage.From);
+  }
+
+  public async Task<Conversation?> GetConversationByUserPhoneNumber(string phoneNumber) {
+    return new Conversation { };
   }
 }
