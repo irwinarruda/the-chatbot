ee7307095ca2f9fff1eaf5dc5bbb916c1daf521c
irwinarruda <arruda.irwin@gmail.com>
2025-11-21 17:09:38 -0300

    feat: add rate limit for migrations endpoint

--- Controllers/MigrationController.cs
+++ Controllers/MigrationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 using TheChatbot.Services;
 
@@ -6,12 +7,14 @@ namespace TheChatbot.Controllers;
 
 [ApiController]
 [Route("/api/v1/[controller]")]
+[EnableRateLimiting("MigrationPolicy")]
 public class MigrationController(MigrationService migrationService) : ControllerBase {
   [HttpGet]
   public async Task<ActionResult> ListPendingMigrations() {
     var migrations = await migrationService.ListPendingMigrations();
     return Ok(migrations);
   }
+
   [HttpPost]
   public async Task<ActionResult> RunPendingMigrations() {
     var password = Request.Headers["X-Migration-Password"].ToString();
